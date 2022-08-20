90595a020b31fda8ff6dbe1bf01c015fb5acaef4
Leandro Marceddu <leandromarceddu@gmail.com>
2022-08-20 19:40:44 +0200

    Clean up and checks

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
-//! Blinks the LED on a Pico board
-//!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
 #![no_std]
 #![no_main]
+
+use bit_field::BitField;
 use bsp::entry;
+use bsp::hal::{clocks::init_clocks_and_plls, pac, sio::Sio, watchdog::Watchdog};
+use core::ops::RangeInclusive;
 use cortex_m::prelude::_embedded_hal_blocking_spi_Transfer;
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::digital::v2::{InputPin, OutputPin};
+use embedded_hal::PwmPin;
 use embedded_time::fixed_point::FixedPoint;
 use embedded_time::rate::Extensions;
-//use max31855::*;
-use bit_field::BitField;
-use bsp::hal::{clocks::init_clocks_and_plls, pac, sio::Sio, watchdog::Watchdog};
-use core::ops::RangeInclusive;
-use embedded_hal::PwmPin;
 use panic_probe as _;
 use rp2040_hal as hal;
 use rp2040_hal::clocks::Clock;
@@ -22,16 +19,6 @@ use rp2040_hal::clocks::Clock;
 use rp_pico as bsp;
 
 const THERMOCOUPLE_BITS: RangeInclusive<usize> = 2..=15;
-/// The minimum PWM value we want
-const LOW: u16 = 0;
-
-/// The maximum PWM value we want
-const HIGH: u16 = 65535;
-
-/// External high-speed crystal on the Raspberry Pi Pico board is 12 MHz. Adjust
-/// if your board has a different frequency
-const XTAL_FREQ_HZ: u32 = 12_000_000u32;
-
 const SETPOINT: f32 = 1000.0;
 
 #[entry]
@@ -42,7 +29,6 @@ fn main() -> ! {
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let sio = Sio::new(pac.SIO);
 
-    // External high-speed crystal on the pico board is 12Mhz
     let external_xtal_freq_hz = 12_000_000u32;
     let clocks = init_clocks_and_plls(
         external_xtal_freq_hz,
@@ -90,32 +76,41 @@ fn main() -> ! {
     // Output channel A on PWM7 to GPIO 14
     let channel = &mut pwm.channel_a;
     channel.output_to(pins.gpio14);
+
+    // Check-loops
+    let switch = pins.gpio17.into_pull_up_input();
     loop {
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        if switch.is_high().unwrap() {
+            info!("Switch NOK");
+            delay.delay_ms(100);
+        } else {
+            info!("Switch OK");
+            led_pin.set_high().unwrap();
+            delay.delay_ms(500);
+            led_pin.set_low().unwrap();
+            delay.delay_ms(500);
 
-        cs_pin.set_low().unwrap();
-        let mut buf: [u8; 2] = [0, 0];
-        delay.delay_ms(10);
-        spi.transfer(&mut buf).unwrap();
-        cs_pin.set_high().unwrap();
-        let raw = (buf[0] as u16) << 8 | (buf[1] as u16);
-        let thermocouple = convert(bits_to_i16(raw.get_bits(THERMOCOUPLE_BITS), 14, 4, 2));
-        info!("term {}", thermocouple);
+            cs_pin.set_low().unwrap();
+            let mut buf: [u8; 2] = [0, 0];
+            delay.delay_ms(10);
+            spi.transfer(&mut buf).unwrap();
+            cs_pin.set_high().unwrap();
+            let raw = (buf[0] as u16) << 8 | (buf[1] as u16);
+            let thermocouple = convert(bits_to_i16(raw.get_bits(THERMOCOUPLE_BITS), 14, 4, 2));
+            info!("term {}", thermocouple);
 
-        if thermocouple <= SETPOINT - 5.0 {
-            channel.set_duty(65535);
-            info!("full power");
-        }
-        if (SETPOINT - 5.0..SETPOINT).contains(&thermocouple) {
-            channel.set_duty(32767);
-            info!("half power");
-        }
-        if thermocouple >= SETPOINT {
-            channel.set_duty(0);
-            info!("no power");
+            if thermocouple <= SETPOINT - 20.0 {
+                channel.set_duty(65535);
+                info!("full power");
+            }
+            if (SETPOINT - 20.0..SETPOINT).contains(&thermocouple) {
+                channel.set_duty(32767);
+                info!("half power");
+            }
+            if thermocouple >= SETPOINT {
+                channel.set_duty(0);
+                info!("no power");
+            }
         }
     }
 }
